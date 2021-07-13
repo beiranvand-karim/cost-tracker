76fca424d048a5589061a8d0639d6c7fb510162e
samshams97 <samshams733@gmail.com>
2021-07-13 14:38:45 +0430

    deleted final unec

--- src/Modal.js
+++ src/Modal.js
@@ -1,20 +1,13 @@
  import React from 'react'
  
-
- 
-
-
  export default function Modal({open,children,onClose}){
+
      if(!open) return null
+
      return (
-
-
             <div >
                 <button onClick={onClose}>close Modal</button>
                 {children}
             </div>
-            
-      
-
      )
  }
