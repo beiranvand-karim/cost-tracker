691328419b66147df99a3057064cd17cf7a5fa10
samshams97 <samshams733@gmail.com>
2021-07-25 20:39:51 +0430

    added new table

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { StylesProvider } from '@material-ui/core/styles';
 
 ReactDOM.render(
+  
   <React.StrictMode>
+    <StylesProvider injectFirst>
     <App />
+    </StylesProvider>
   </React.StrictMode>,
   document.getElementById('root')
+  
 );
