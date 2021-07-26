8ffd044f4a425dedf365204d8108aeed78e0c5ed
samshams97 <60235409+samshams97@users.noreply.github.com>
2021-07-26 20:39:44 +0430

    used prettier to make code clean (#21)
    * used prettier to make code clean
    
    * new changes added

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -20,7 +20,7 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import { useState } from 'react';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -39,38 +39,33 @@ const useStyles = makeStyles((theme) =>
       margin: theme.spacing(1),
       minWidth: 120,
     },
-    testButton:{
-      position:'absolute',
-      right:-16,
-      bottom:-16,
+    testButton: {
+      position: 'absolute',
+      right: -16,
+      bottom: -16,
       transform: 'translate(-50%, -50%)',
-      marginTop:40
+      marginTop: 40,
     },
-    tableStyle:{
+    tableStyle: {
       display: 'flex',
       justifyContent: 'center',
-      alignItems:'center',
-      alignContent:'center',
-      flexDirection:'column',
-      marginTop:40,
-      
+      alignItems: 'center',
+      alignContent: 'center',
+      flexDirection: 'column',
+      marginTop: 40,
     },
-    costStyle:{
-      alignSelf:'center',
-      textAlign:'center'
-
+    costStyle: {
+      alignSelf: 'center',
+      textAlign: 'center',
     },
-    catgryStyle:{
-      order:1
+    catgryStyle: {
+      order: 1,
     },
-    dscrpStyle:{
-      order:2
+    dscrpStyle: {
+      order: 2,
     },
-    bodyStyle:{
-      
-    }
- 
-  }),
+    bodyStyle: {},
+  })
 );
 const useStyles2 = makeStyles({
   table: {
@@ -78,70 +73,72 @@ const useStyles2 = makeStyles({
   },
 });
 const showModal1 = styled(Modal)`
-   display:flex;
-   justify-content: center;
-   align-items: center;
- `;
-
+  && {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 const App = () => {
-  const [annaCost,setAnnaCost] =useState( [])
-  
-  const[open,setOpen] = useState(false);
+  const [annaCost, setAnnaCost] = useState([]);
 
-  const [title,setTitle] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const [title2,setTitle2] = useState('');
+  const [title, setTitle] = useState('');
 
-  const [title3,setTitle3] = useState('');  
+  const [title2, setTitle2] = useState('');
 
-  const classes = useStyles(); 
-  const classes2 = useStyles2();
+  const [title3, setTitle3] = useState('');
 
-  const handleSubmit=(e)=>{
+  const classes = useStyles();
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setAnnaCost(values=>setAnnaCost([...values,{costItem:title,category:title2,description:title3}]))
-    setOpen(false)
-  }
-
+    setAnnaCost((values) =>
+      setAnnaCost([
+        ...values,
+        { costItem: title, category: title2, description: title3 },
+      ])
+    );
+    setOpen(false);
+  };
 
   return (
     <div className="App">
-
-<TableContainer component={Paper}>
-      {annaCost.length> 0 && ( <Table className={classes.tableStyle} hover >
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">costItem</TableCell>
-            <TableCell align="right">category</TableCell>
-            <TableCell align="right">description</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {annaCost.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.costItem}</TableCell>
-              <TableCell align="right">{row.category}</TableCell>
-              <TableCell align="right">{row.description}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table> )}
-    </TableContainer>
-      <Fab className={classes.testButton}
-      onClick={()=> setOpen(true)} >
-        <AddIcon/>
+      <TableContainer component={Paper}>
+        {annaCost.length > 0 && (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">costItem</TableCell>
+                <TableCell align="right">category</TableCell>
+                <TableCell align="right">description</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {annaCost.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.costItem}</TableCell>
+                  <TableCell>{row.category}</TableCell>
+                  <TableCell>{row.description}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </TableContainer>
+      <Fab className={classes.testButton} onClick={() => setOpen(true)}>
+        <AddIcon />
       </Fab>
-      
-      <Modal
+
+      <showModal1
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         className={classes.modal}
         open={open}
-        onClose={()=> setOpen(false)}
+        onClose={() => setOpen(false)}
         closeAfterTransition
         BackdropComponent={Backdrop}
         BackdropProps={{
@@ -150,56 +147,66 @@ const App = () => {
       >
         <Fade in={open}>
           <div className={classes.paper}>
-            <form  >
-            <Typography>
-              <TextField
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              fullWidth
-              variant="outlined"
-              margin="20"
-              label="Cost Item" />
+            <form>
+              <Typography>
+                <TextField
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  fullWidth
+                  variant="outlined"
+                  margin="20"
+                  label="Cost Item"
+                />
 
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="grouped-native-select">Grouping</InputLabel>
-        <Select native defaultValue="" id="grouped-native-select"
-        value={title2} onChange={(e)=>setTitle2(e.target.value)}
-         >
-            <option value="food">  food </option>
-            <option value="clothes" > clothes </option>
-            <option value="furniture" > furniture </option>
-            <option value="gym" > gym </option>
-          
-        </Select>
-      </FormControl>
-     <div>
-     <TextareaAutosize  value={title3} 
-     onChange={(e)=>setTitle3(e.target.value)} aria-label="empty textarea" placeholder="Empty" />
-     </div>
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="grouped-native-select">
+                    Grouping
+                  </InputLabel>
+                  <Select
+                    native
+                    defaultValue=""
+                    id="grouped-native-select"
+                    value={title2}
+                    onChange={(e) => setTitle2(e.target.value)}
+                  >
+                    <option value="food"> food </option>
+                    <option value="clothes"> clothes </option>
+                    <option value="furniture"> furniture </option>
+                    <option value="gym"> gym </option>
+                  </Select>
+                </FormControl>
+                <div>
+                  <TextareaAutosize
+                    value={title3}
+                    onChange={(e) => setTitle3(e.target.value)}
+                    aria-label="empty textarea"
+                    placeholder="Empty"
+                  />
+                </div>
 
-     <div className="btn-style" >
-     <Button
-      variant="contained"
-      onClick={handleSubmit}
-      className={classes.customStyle}
-      
-      >
-        Add
-      </Button>
-      <Button onClick ={()=> setOpen(false)}
-      variant="contained"
-      className={classes.customStyle} >
-        Cancel
-      </Button>
-     </div>
-            </Typography>
+                <div className="btn-style">
+                  <Button
+                    variant="contained"
+                    onClick={handleSubmit}
+                    className={classes.customStyle}
+                  >
+                    Add
+                  </Button>
+                  <Button
+                    onClick={() => setOpen(false)}
+                    variant="contained"
+                    className={classes.customStyle}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </Typography>
             </form>
-            
           </div>
         </Fade>
-      </Modal> 
+      </showModal1>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { StylesProvider } from '@material-ui/core/styles';
 
 ReactDOM.render(
-  
   <React.StrictMode>
     <StylesProvider injectFirst>
-    <App />
+      <App />
     </StylesProvider>
   </React.StrictMode>,
   document.getElementById('root')
-  
 );
